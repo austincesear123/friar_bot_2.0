6163be9fc4815ec218ba39417077e3e286f1af55
Austin Cesear <austincesear123@gmail.com>
2022-05-09 21:18:45 -0700

    add fetch player image

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,11 +1,25 @@
-const randomImage = require("./imageSearch");
+const players = require("./players");
+const fetch = require("node-fetch");
 
-function handleMessage(message) {
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+async function handleMessage(message) {
   const stringMessage = message.toString();
-  const newMessage = stringMessage.substring(1);
-  if (stringMessage.startsWith("-") === true) {
-    if (newMessage === "tatis") {
-      randomImage.randomImage(message);
+  const subStringMessage = message.toString().substring(1);
+  if (stringMessage.startsWith("-")) {
+    for (i = 0; i < players.players.length; i++) {
+      if (players.players[i] === subStringMessage) {
+        await fetch(`http://127.0.0.1:3000/friar_bot/get/${players.players[i]}`)
+          .then((res) => res.json())
+          .then((data) =>
+            message.channel.send(
+              `${data.playerImgs[randomInt(1, data.playerImgs.length) - 1].url}`
+            )
+          )
+          .catch((error) => console.log(error));
+      }
     }
   }
 }
